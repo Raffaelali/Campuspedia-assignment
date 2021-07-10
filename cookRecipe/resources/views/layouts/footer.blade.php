171957fcdb2c4bf209b7a131a3390294485aa1db
Raff <raff.hizz@gmail.com>
2021-07-10 16:46:26 +0700

    main footer

---
+++ cookRecipe/resources/views/layouts/footer.blade.php
@@ -0,0 +1,25 @@
+<!-- Footer -->
+<footer class="text-center text-white" style="background-color: #a0c3e4;">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+    <!-- Section: CTA -->
+    <section class="">
+        <p class="d-flex justify-content-center align-items-center">
+        <span class="me-3">Are u a chef?</span>
+        <button type="button" class="btn btn-outline-light btn-rounded">
+            Start making recipe!
+        </button>
+        </p>
+    </section>
+    <!-- Section: CTA -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: #a0c3e4;">
+    © 2020 Copyright:
+    <a class="text-white" href="{{ route('home') }}">CookRecipe.com</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
