b2a33a20686cd97892b68aea5de60c0a8f1cefc9
Raff <raff.hizz@gmail.com>
2021-07-11 12:21:25 +0700

    add and grouping routes

--- cookRecipe/routes/web.php
+++ cookRecipe/routes/web.php
@@ -20,4 +20,11 @@ Route::get('/sign', 'SignController@index')->name('Sign');
 
 // halaman khusus para chef
 Route::middleware('auth')->group(function () {
+    Route::get('/recipes', 'RecipeController@index')->name('Recipes.index');
+    Route::get('/recipes/make', 'RecipeController@create')->name('Recipes.create');
+    Route::post('/recipes', 'RecipeController@store')->name('Recipes.store');
+    Route::delete('/recipes/{recipe}', 'RecipeController@destroy')->name('Recipes.delete');
+    Route::get('/recipes/{recipe}/edit', 'RecipeController@edit')->name('Recipes.edit');
+    Route::patch('/recipes/{recipe}', 'RecipeController@update')->name('Recipes.update');
+    Route::get('/recipes/{recipe}', 'RecipeController@show')->name('Recipes.show');
 });
